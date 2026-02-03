2c043fc4a3b93d11892a556fc1e8236278b67e57
katttiger <katttiger@gmail.com>
2026-02-03 10:59:39 +0100

    Initial commit

---
+++ src/test/java/se/iths/cecilia/atm/AtmApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.cecilia.atm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
