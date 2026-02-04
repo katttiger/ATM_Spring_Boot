b277e0fde8546731446405a7b3df0542d157c487
katttiger <katttiger@gmail.com>
2026-02-04 11:54:28 +0100

    Added tests using Playwright to test the page being reached, loaded properly and the balance being correct.

--- src/test/java/se/iths/cecilia/atm/controller/ATMControllerTest.java
+++ src/test/java/se/iths/cecilia/atm/controller/ATMControllerTest.java
@@ -1,56 +1,81 @@
 package se.iths.cecilia.atm.controller;
 
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.BrowserContext;
-import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.*;
 import org.junit.jupiter.api.*;
+import se.iths.cecilia.atm.models.AccountComponent;
+import se.iths.cecilia.atm.services.ATMService;
+
+import java.util.regex.Pattern;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 class ATMControllerTest {
 
+
+    // Shared between all tests in this class.
+    static Playwright playwright;
+    static Browser browser;
+
+    // New instance for each test method.
+    BrowserContext context;
+    Page page;
+
     // TODO: Läs på om playwright
     // TODO: Skriv testerna
-    private static Page page;
-    private static Browser browser;
-    private static BrowserContext context;
+
 
     @BeforeAll
     static void launchBrowser() {
-        Playwright playwright = Playwright.create();
+        playwright = Playwright.create();
         browser = playwright.chromium().launch();
     }
 
     @AfterAll
     static void closeBrowser() {
-        browser.close();
+        playwright.close();
     }
 
     @BeforeEach
-    void setup() {
+    void createContextAndPage() {
         context = browser.newContext();
-        page = browser.newPage();
+        page = context.newPage();
     }
 
     @AfterEach
-    void tearDown() {
+    void closeContext() {
         context.close();
     }
 
+
     @Test
-    @DisplayName("The page can be reached")
+    @DisplayName("The root-page can be reached")
     void pageCanBeReached() {
+        page.navigate("http://localhost:8080/");
+        assertThat(page).hasTitle(Pattern.compile("Balance"));
     }
 
     @Test
-    @DisplayName("The page is loaded correctly")
-    void pageIsLoadedCorrectly() {
+    @DisplayName("The table is loaded correctly")
+    void pageTableIsLoadedCorrectly() {
         page.navigate("http://localhost:8080/");
-
+        Locator tableLocator = page.locator("#balance-table");
+        assertThat(tableLocator).isVisible();
     }
+
 
     @Test
     @DisplayName("The amount displayed is correct")
     void amountDisplayedIsCorrect() {
+        ATMService service = new ATMService(new AccountComponent());
+        int expectedAmount = service.getBalance();
+        page.navigate("http://localhost:8080/");
+
+        Locator balanceLocator = page.locator("#balance");
+        String balanceAsText = balanceLocator.textContent().trim();
+
+        int balanceAsInt = Integer.parseInt(balanceAsText);
+
+        Assertions.assertEquals(expectedAmount, balanceAsInt);
 
     }
 }
